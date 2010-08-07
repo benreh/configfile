f5954cf02c5a6ed620e6a67b17993396afe4c387
Benjamin Reh <ich@benjaminreh.de>
2010-08-07 12:53:11 +0200

    GPL
    GPL Licence added
    
    Signed-off-by: Benjamin Reh <ich@benjaminreh.de>

--- main.cpp
+++ main.cpp
@@ -1,3 +1,19 @@
+/*	Copyright 2010 Benjamin Reh <ich@benjaminreh.de>
+*
+*	This file is part of 'configfile'.
+*
+*    'configfile' is free software: you can redistribute it and/or modify
+*    it under the terms of the GNU General Public License as published by
+*    the Free Software Foundation version 3 of the License.
+*
+*    'configfile' is distributed in the hope that it will be useful,
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*    GNU General Public License for more details.
+*
+*    You should have received a copy of the GNU General Public License
+*    along with 'configfile'.  If not, see <http://www.gnu.org/licenses/>.
+*/
 #include <iostream>
 #include "configfile.h"
 using namespace std;
